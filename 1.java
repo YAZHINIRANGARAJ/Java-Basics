dab3ad01f8124a82b7d34e94a959dfeb19ba929a
YAZHINIRANGARAJ <yazhinirangaraj@gmail.com>
2017-07-25 23:03:30 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Yazh
+
+{
+    
+    public void dis(int age)
+    {
+        System.out.println("Yazh's age is" + age);
+        
+    }
+    public static void main(String[] args)
+    {
+        Yazh yal=new Yazh();
+        yal.dis(20);
+    }
+}
